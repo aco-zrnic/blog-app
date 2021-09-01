8bc4c2e00a79c8a85e170de3e1dea7273e31325a
aco-zrnic <60435330+aco-zrnic@users.noreply.github.com>
2021-09-01 15:17:45 +0200

    init blog app

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import IndexScreen from './src/screens/IndexScreen';
+
+const Stack = createStackNavigator();
+
+function MyStack(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Index'
+        component={IndexScreen}
+      />
+    </Stack.Navigator>
+  )
+}
+
 
 export default function App() {
   return (
